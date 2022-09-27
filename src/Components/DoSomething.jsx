beb4b0afe31af534a41b342d68e6645243ced975
Gracie Fogarty <graciealysse@gmail.com>
2022-09-27 13:21:33 -0700

    Completed DoSomething form

--- src/Components/DoSomething.jsx
+++ src/Components/DoSomething.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 
 function DoSomething() {
   const [activity, setActivity] = useState('');
   const [accessibility, setAccessibility] = useState('');
-  const [selectedAccessibility, setSelectedAccessibility] = useState('');
+  const [selectedAccessibility, setSelectedAccessibility] = useState(undefined);
   const [type, setType] = useState('');
   const [participants, setParticipants] = useState('');
   const [price, setPrice] = useState('');
@@ -18,13 +18,42 @@ function DoSomething() {
   };
 
   function selectAccessibility (value) {
-    console.log(value);
-    if(selectedAccessibility) {
-      setSelectedAccessibility('');
+    if(selectedAccessibility !== undefined) {
+      setSelectedAccessibility(undefined);
     } else {
       setSelectedAccessibility(0.0);
     }
   }
+
+  function updateActivity (e) {
+    e.preventDefault();
+    console.log(type);
+    console.log(selectedAccessibility);
+    if(selectedAccessibility === 0) {
+      axios.get(`http://www.boredapi.com/api/activity?type=${type}&accessibility=0.0`)
+      .then((response) => {
+        console.log(response);
+        if(response.data.error) {
+          setActivity(response.data.error)
+        } else {
+          setActivity(response.data.activity);
+          setAccessibility(response.data.accessibility);
+          setParticipants(response.data.participants);
+          setPrice(response.data.participants);
+        }
+      })
+      .catch();
+    } else {
+      axios.get(`http://www.boredapi.com/api/activity?type=${type}`)
+      .then((response) => {
+        setActivity(response.data.activity);
+        setAccessibility(response.data.accessibility);
+        setParticipants(response.data.participants);
+        setPrice(response.data.participants);
+      })
+    }
+  }
+
 
   return (
     <div className="do-something">
@@ -56,13 +85,15 @@ function DoSomething() {
               <input type="checkbox" value="no" />
             </label>
           </label>
+          <br></br>
+          <br></br>
+          <button type="submit" onClick={(e) => updateActivity(e)}>Find me a new activity!</button>
         </form>
       </div>
       <br></br>
       <div>
         <h4>Suggested Activity</h4>
-        <span>activity: {activity}</span>
-        <br></br>
+        <h5>{activity}</h5>
         <br></br>
         <span>accessibility score (0 - 1): {accessibility}</span>
         <br></br>
