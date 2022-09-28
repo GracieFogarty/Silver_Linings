8134c48d401c9a3c2b55ac0ddb861814901a2d7e
Gracie Fogarty <graciealysse@gmail.com>
2022-09-27 17:19:28 -0700

    Added fox pics from random fox api

--- src/Components/LookAtSomething.jsx
+++ src/Components/LookAtSomething.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-// import axios from 'axios';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function LookAtSomething() {
+  const [photo, setPhoto] = useState('');
+
+  function getCat(e) {
+    e.preventDefault();
+    axios.get('https://randomfox.ca/floof/')
+    .then((response) => {
+      setPhoto(response.data.image);
+    })
+    .catch()
+  }
+
   return (
     <div className="look-at-something">
       <h3>Look at something cute!</h3>
-
+      <br></br>
+      <button type="submit" onClick={(e) => getCat(e)}>Get something cute!</button>
+      <img src={photo} alt="cute fox pic"></img>
     </div>
   );
 }
